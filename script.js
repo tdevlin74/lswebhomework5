1a959d59432df7ed4b66597c47826ccde56eb0a9
tdevlin74 <tdevlin74@gmail.com>
2017-06-12 19:47:12 -0400

    hw completed

--- script.js
+++ script.js
@@ -1,34 +1,110 @@
 //1. print the number 5 to the console
 
+console.log(5);
+
 //2. Print your name to the console
+
+console.log("Taylor");
 
 //3. Store your age as a variable called "myAge"
 
+var myAge = 27;
+
 //4. Print to the console how old you will be in 5 years
+
+console.log(myAge + 5);
 
 //5. Store your favorite food as a variable called "myFavoriteFood"
 
+var myFavoriteFood = "burrito";
+
 //6. Publish your favorite food to `index.html` using `document.write()`
+
+document.write(myFavoriteFood + "<br>");
 
 //7. Print the remainder of 14 / 3 to the console
 
+console.log(14 % 3);
+
 //8. Print the remainder of 829 / 13 to the console
+
+console.log(829 % 13);
 
 //9. Create a for loop that counts from 0 to 130 by 3s
 
+for(var i = 0; i <= 130; i += 3){
+    document.write(i + " ");
+}
+document.write("<br>");
+
 //10. Create a for loop that counts from 3 to 17 by 2s
+
+for(var i = 3; i <= 17; i += 2){
+    document.write(i + " ");
+}
+document.write("<br>");
 
 //11. Create a for loop that counts from 100 to 3 by -1
 
+for(var i = 100; i >= 3; i--){
+    document.write(i + " ");
+}
+document.write("<br>");
+
 //12. Create a for loop that counts from 1 to 100 by 1s
+
+for(var i = 1; i <= 100; i++){
+    document.write(i + " ");
+}
+document.write("<br>");
 
 //13. Create a for loop that counts from 1 to 100, but instead of printing `i` prints `fizz` if the number is divisible by 5
 
+for(var i = 1; i <= 100; i++){
+    if(i % 5 === 0){
+        document.write("fizz" + " ");
+    } else {
+        document.write(i + " ");
+    }
+}
+document.write("<br>");
+
 //14. Create a for loop that counts from 1 to 100, but instead of printing `i` prints `buzz` if the number is divisible by 3
+
+for(var i = 1; i <= 100; i++){
+    if(i % 3 === 0){
+        document.write("buzz" + " ");
+    } else {
+        document.write(i + " ");
+    }
+}
+
+document.write("<br>");
 
 //15. Create a for loop that counts from 1 to 100, but instead of printing `i` prints `fizzbuzz` if the number is divisible by 15
 
+for(var i = 1; i <= 100; i++){
+    if(i % 15 === 0){
+        document.write("fizzbuzz" + " ");
+    } else {
+        document.write(i + " ");
+    }
+}
+document.write("<br>");
+
 //EXTRA CREDIT: Fizzbuzz
+
+for(var i = 1; i <= 100; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+        document.write("FizzBuzz" + " ");
+    } else if(i % 5 === 0 ){
+        document.write("Buzz" + " ");
+    } else if(i % 3 === 0){
+        document.write("Fizz" + " ");
+    } else {
+        document.write(i + " ");
+    }
+}
 
 /*
 
